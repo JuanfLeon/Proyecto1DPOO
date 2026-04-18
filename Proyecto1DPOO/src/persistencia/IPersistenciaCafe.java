ed8a9879caa6a0726c0f67c64af637527e10410e
JuandaVasquez <jd.vasquezl1@uniandes.edu.co>
2026-04-18 16:32:08 -0500

    Persistencia

---
+++ Proyecto1DPOO/src/persistencia/IPersistenciaCafe.java
@@ -0,0 +1,5 @@
+package persistencia;
+
+public interface IPersistenciaCafe {
+
+}
---
+++ Proyecto1DPOO/src/persistencia/PersistenciaTiendaDeJuegosSerializacion.java
@@ -0,0 +1,12 @@
+package persistencia;
+
+public class PersistenciaTiendaDeJuegosSerializacion {
+
+	public void cargarTiendaDeJuegos() {
+		
+	}
+	
+	public void guardarTiendaDeJuegos() {
+		
+	}
+}
